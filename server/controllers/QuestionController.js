c7ed3dd1a333217c18da5e9ca4d19405b4252f4b
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2020-07-23 21:28:57 +0700

    + add readbyid for question

--- server/controllers/QuestionController.js
+++ server/controllers/QuestionController.js
@@ -8,6 +8,15 @@ class QuestionController {
       return res.status(500).json({ message: "Internal Error Service" });
     }
   }
+  static async readById(req, res) {
+    try {
+      const getId = req.params.id;
+      const result = await Question.findOne({ where: { id: getId } });
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(500).json({ message: "Internal Error Service" });
+    }
+  }
 }
 
 module.exports = QuestionController;
