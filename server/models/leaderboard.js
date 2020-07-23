d5080c66ed7c14b701a73cb9c300363475692b12
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2020-07-23 18:46:02 +0700

    inisiasi group project 4 : gambar tebak add folder client & server npm i for starting project (client and server)

---
+++ server/models/leaderboard.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Leaderboard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Leaderboard.init({
+    name: DataTypes.STRING,
+    score: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Leaderboard',
+  });
+  return Leaderboard;
+};
