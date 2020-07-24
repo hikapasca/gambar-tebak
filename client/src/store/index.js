f5fa0d22d7988afec8ee905d6dce6b3f7845dfaa
Mayana <mayanafitri25@gmail.com>
2020-07-24 13:42:53 +0700

    Configuration of socket in Client side

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -12,7 +12,8 @@ export default new Vuex.Store({
     questions: [],
     allAnswers: [],
     finalScore: [],
-    username: ''
+    username: '',
+    dataUser: []
   },
   mutations: {
     ADD_USER(state, payload) {
@@ -38,11 +39,23 @@ export default new Vuex.Store({
         }
       });
     },
+    ADDED_SCORE(state, payload) {
+      state.dataUser = payload
+      console.log(state.dataUser, `ini show all`)
+    },
+    UPDATED_SCORE(state, payload) {
+      // for (let i = 0; i < state.dataUser.length; i++) {
+      //   if (state.dataUser[i].name === payload.name) {
+      //     state.dataUser[i].score += payload.score;
+      //   }
+      // }
+    },
     RESET_ANSWER(state) {
       state.allAnswers = []
     },
     RESET_SCORE(state) {
       state.finalScore = []
+      state.dataUser = []
     }
   },
   actions: {
@@ -58,6 +71,65 @@ export default new Vuex.Store({
         .catch((err) => {
           console.log(err.response);
         });
+    },
+    updateLeaderboard(context, payload) {
+      // console.log("update masuk didalam");
+      // const score = payload.score;
+      const updateLeaderboard = {
+        name: payload.name,
+        score: payload.score
+      };
+      console.log(updateLeaderboard, `ini yg mau update`)
+      axios({
+        method: "PUT",
+        url: "http://localhost:3000/leaderboards",
+        data: updateLeaderboard
+      })
+        .then(result => {
+          console.log("update masuk diluar");
+          // context.commit("UPDATED_SCORE", updateLeaderboard);
+        })
+        .catch(err => {});
+    },
+    deleteLeaderboard(context, payload) {
+      // console.log("update masuk didalam");
+      axios({
+        method: "DELETE",
+        url: "http://localhost:3000/leaderboards"
+      })
+        .then(result => {
+          // console.log("update masuk diluar");
+          context.commit("RESET_SCORE");
+        })
+        .catch(err => {});
+    },
+    addLeaderboard(context, payload) {
+      // console.log("add masuk diluar");
+      const newLeaderboard = {
+        name: payload.name,
+        score: payload.score
+      };
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/leaderboards",
+        data: newLeaderboard
+      })
+        .then(result => {
+          // console.log("add masuk didalam");
+          // context.commit("ADDED_SCORE", newLeaderboard);
+        })
+        .catch(err => {});
+    },
+    showLeaderboard(context) {
+      axios({
+        method: "GET",
+        url: "http://localhost:3000/leaderboards"
+      })
+        .then(result => {
+          // console.log("add masuk didalam");
+          context.commit("ADDED_SCORE", result.data);
+        })
+        .catch(err => {});
     }
   },
   modules: {},
--- client/src/views/LeaderBoard.vue
+++ client/src/views/LeaderBoard.vue
@@ -1,36 +1,49 @@
 <template>
     <div id="leaderboard">
-        <div id="leaderHeader">
-            <h1>Tertinggi saat ini</h1>
-            <button @click="balikLobby" class="btn btn-primary">Balik Lobby</button>
-        </div>
-        <div id="leaderTable">
-            <table class="table table-dark">
-                <thead>
-                    <tr>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <LeaderData v-for="(user, index) in $store.state.finalScore" :key="index" :score="user"></LeaderData>
-                </tbody>
-            </table>
+        <div id="boxLeader">
+            <div id="leaderHeader">
+                <h1>Hasil Kerecehan</h1>
+                <button @click="balikLobby" class="btn btn-primary">Balik Lobby</button>
+            </div>
+            <div id="leaderTable">
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <LeaderData v-for="(user, index) in $store.state.finalScore" :key="index" :score="user"></LeaderData>
+                    </tbody>
+                </table>
+                    <p>{{this.$store.state.dataUser}}</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import LeaderData from '../components/LeaderData.vue';
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 export default {
   name: 'LeaderBoard',
   components: {
     LeaderData,
   },
+  created() {
+    //   socket.on("deleteScore",  () => {
+    //     this.$store.dispatch('deleteLeaderboard')
+    // })
+    this.$store.dispatch('showLeaderboard')
+  },
   methods: {
     balikLobby() {
-        this.$store.commit('RESET_SCORE')
+        // this.$store.commit('RESET_SCORE')
+        // socket.emit("deleteScore")
+        this.$store.dispatch('deleteLeaderboard')
         this.$router.push({name: 'Lobby'})
     }
   }
